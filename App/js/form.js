fda8a78ea19e5557e3cb4154d7fc8d068c644d1b
NjimsRane <njims2011@gmail.com>
2023-05-01 09:36:30 +0100

    changed font family

--- App/js/form.js
+++ App/js/form.js
@@ -1,24 +1,42 @@
-const registerForm = document.querySelector('#register-form');
+const loginForm = document.querySelector('#form-login');
 const emailLogin = document.querySelector('#login-email');
 const emailLoginError = document.querySelector('#email-login-error');
 const passLogin = document.querySelector('#login-pass');
 const passLoginError = document.querySelector('#pass-login-error');
+const showPasswordBtn = document.querySelector('.hide-show-password');
+const eyeImage = document.querySelector('.eye-img');
 
-registerForm.addEventListener('submit', (e) => {
+
+loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     if (emailLogin.value === '' && passLogin.value === '') {
-        emailLoginError.textContent = 'Please enter you email';
-        passLoginError.textContent = 'Please enter you password';
+        emailLoginError.textContent = 'Please enter your email';
+        passLoginError.textContent = 'Please enter your password';
+        emailLogin.classList.add('showError');
     }
     else if (emailLogin.value !== '' && passLogin.value === '') {
         emailLoginError.textContent = '';
-        passLoginError.textContent = 'Please enter you password';
+        passLoginError.textContent = 'Please enter your password';
     }
     else if (emailLogin.value === '' && passLogin.value !== '') {
-        emailLoginError.textContent = 'Please enter you email';
+        emailLoginError.textContent = 'Please enter your email';
         passLoginError.textContent = '';
     } else {
         emailLoginError.textContent = '';
         passLoginError.textContent = '';
+    }
+});
+
+
+
+showPasswordBtn.addEventListener('click', function () {
+    if (passLogin.value === "") {
+        return;
+    } else if (passLogin.type === 'password') {
+        passLogin.type = 'text';
+        eyeImage.src = '/Images/pass/show.png';
+    } else {
+        passLogin.type = 'password';
+        eyeImage.src = '/Images/pass/hide.png';
     }
 });
