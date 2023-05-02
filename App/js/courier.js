5e112e74acacd53b6201156024f5a03013914ad8
NjimsRane <njims2011@gmail.com>
2023-05-02 16:43:25 +0100

    styled the contact page.

--- App/js/courier.js
+++ App/js/courier.js
@@ -50,18 +50,3 @@ const courier = () => {
 };
 courier();
 
-
-window.addEventListener('scroll', reveal);
-
-function reveal() {
-    let reveals = document.querySelectorAll('.reveal');
-
-    for (let i = 0; i < reveals.length; i++) {
-        let windowHeight = window.innerHeight;
-        let revealTop = reveals[i].getBoundingClientRect().top;
-        let revealPoint = 150;
-        (revealTop < windowHeight - revealPoint)
-            ? reveals[i].classList.add('active')
-            : reveals[i].classList.remove('active');
-    }
-}
--- App/js/script.js
+++ App/js/script.js
@@ -1,2 +1,18 @@
 const setDate = document.querySelector('#set-date');
 setDate.textContent = new Date().getFullYear();
+
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    let reveals = document.querySelectorAll('.reveal');
+
+    for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+        (revealTop < windowHeight - revealPoint)
+            ? reveals[i].classList.add('active')
+            : reveals[i].classList.remove('active');
+    }
+}
